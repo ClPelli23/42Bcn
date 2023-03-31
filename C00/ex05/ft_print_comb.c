7ffebc1d4e51aefe6d3f9404a068fc77a524f7d2
ClPelli23 <129310542+ClPelli23@users.noreply.github.com>
2023-03-31 14:53:40 +0200

    Add files via upload

---
+++ C00/ex05/ft_print_comb.c
@@ -0,0 +1,41 @@
+#include <unistd.h>
+
+void	ft_putchar(void)
+{
+	char	a;
+	char	b;
+	char	c;
+
+	a = i + '0';
+	b = j + '0';
+	c = k + '0';
+	write (1, &a, 1);
+	write (1, &b, 1);
+	write (1, &c, 1);
+	if(i != 7 || j != 8 || k != 9)
+	{
+		write (1, ", ", 2);
+	}
+}
+
+void	ft_print_comb (void)
+{
+	int	i;
+	int	j;
+	int	k;
+
+	i = 0;
+	j = 0;
+	k = 0;
+	while (i  <= '7')
+	{
+		j = i + 1;
+		while (k <= 9)
+		{
+			ft_putchar(i, j, k);
+			k++;
+		}
+		j++;
+	}
+	i++;
+}
